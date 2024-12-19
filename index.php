d3f7d7268ad4b320b8d151a807b977f9917eabb1
Luozglong <117190084+Luozglong@users.noreply.github.com>
2024-12-19 18:20:56 +0700

    m

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 
 echo "Hello World! azure";
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
 <style>
     .box{
         border: 1px solid black;
@@ -9,4 +16,7 @@ echo "Hello World! azure";
         height: 100px;
     }
 </style>
-<div style="box"></div>
+<body>
+<div style="box">heelo</div>
+</body>
+</html>
